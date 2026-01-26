e13629a40242efb606822b00e408d9dc0b4949c9
hkiruban <harikiruban2006@gmail.com>
2026-01-26 05:03:27 -0500

    Update nvda_filter.sql

--- Basics/Database-Logic/nvda_filter.sql
+++ Basics/Database-Logic/nvda_filter.sql
@@ -1,7 +1,7 @@
 -- You have a table called Stock_Market. It has the following columns: date, ticker, open_price, close_price, and volume.
 -- Write a query to find the date and close_price for 'NVDA' where the volume was greater than 1,000,000.
 
-//Solution:
+-- Solution:
 SELECT date, close_price 
 FROM Stock_Market 
 WHERE ticker = 'NVDA' 
