b533b31599f7371aadf268a1e02591c0ff3ae6a6
hkiruban <harikiruban2006@gmail.com>
2026-01-28 19:31:36 -0500

    Create Basic_Addition.cpp

---
+++ Basics/C++/Basic_Addition.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    int x = 1;
+    int y = 2;
+    
+    // You must declare the type of 'z' before or during assignment
+    int z = x + y; 
+    
+    std::cout << "Result: " << z << std::endl;
+    
+    return 0; // Standard exit code for 'main'
+}
