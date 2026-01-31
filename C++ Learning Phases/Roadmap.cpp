fa7acde63b060b62d7f3efbd6900c91504662b9b
hkiruban <harikiruban2006@gmail.com>
2026-01-31 18:08:19 -0500

    Update and rename C++ Notes to C++ Learning Phases/Roadmap.cpp

---
+++ C++ Learning Phases/Roadmap.cpp
@@ -0,0 +1,8 @@
+// Phase 1: Syntax & Logic  
+// Phase 2: Functions & Scope
+// Phase 3: Pointers & Memory
+// Phase 4: OOP (Classes)
+// Phase 5: The STL
+// Phase 6: Templates & Optimization
+
+// Phase 7: High-Performance Computing, Hardware Awareness and Concurrency (cache locality, multithreading, SIMD)
