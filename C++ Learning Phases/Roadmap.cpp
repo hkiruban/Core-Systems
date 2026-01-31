93d5dafe1133297fcd3bf72e458f44a9704c1128
hkiruban <harikiruban2006@gmail.com>
2026-01-31 18:22:54 -0500

    Update Roadmap.cpp

--- C++ Learning Phases/Roadmap.cpp
+++ C++ Learning Phases/Roadmap.cpp
@@ -1,8 +1,9 @@
-// Phase 1: Syntax & Logic  
-// Phase 2: Functions & Scope
-// Phase 3: Pointers & Memory
-// Phase 4: OOP (Classes)
-// Phase 5: The STL
-// Phase 6: Templates & Optimization
+// Phase 1: Syntax & Logic, Week 1-2
+// Phase 2: Functions & Scope, Week 3-4
+// Phase 3: Pointers & Memory, Week 5-8
+// Phase 4: OOP (Classes), Week 9-12
+// Phase 5: The STL, Week 13-19
+// Phase 6: Templates & Optimization, Week 20-24
+// Phase 6 Forcasted Completion Date End of June 2026
 
 // Phase 7: High-Performance Computing, Hardware Awareness and Concurrency (cache locality, multithreading, SIMD)
