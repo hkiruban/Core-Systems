f4c9cd114747fa958cb8d1994b2eb59ee07bc975
hkiruban <harikiruban2006@gmail.com>
2026-02-01 06:33:52 -0500

    Create Logic.cpp

---
+++ C++ Learning Phases/Phase-1/Logic.cpp
@@ -0,0 +1,21 @@
+/* 
+Before the main logic starts, you must include the libraries that give the program its 'senses' and 'tools'
+
+#include <iostream>: For input and output (cin and cout). 
+
+#include <cmath>: Essential for the sqrt() function. You aren't calculating square roots by hand; use the library.
+
+
+
+Standard C++ functions live in the std namespace.
+
+The Disciplined Way: Use std::cout and std::cin.
+
+The Shortcut: using namespace std; (Common for beginners, 
+  but in larger projects like your Optimization Engine, 
+  it can cause naming conflicts. 
+  Learn to use the std:: prefix now to build better habits).
+
+
+  By convention, the main function should return 0 when finished
+*/
