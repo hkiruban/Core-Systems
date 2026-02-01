653783111461810e30c875f26f3c12ccbf5cbafb
hkiruban <harikiruban2006@gmail.com>
2026-02-01 06:17:17 -0500

    Create Variable-Initialization.cpp

---
+++ C++ Learning Phases/Phase-1/Variable-Initialization.cpp
@@ -0,0 +1,8 @@
+/* 
+Type	      Purpose                          	                Size (Typical)
+int	        Whole numbers (Counting tasks/iterations)	        4 bytes
+double	    High-precision decimals (GPA/Optimization math)	  8 bytes
+float	      Lower-precision decimals (Rarely used in Quants)	4 bytes
+char	      Single characters (Grade letters: 'A', 'B')	      1 byte
+bool	      Truth values (true or false)	                    1 byte 
+*/
