2082edde39b58448109298f66819b362e4b11fac
hkiruban <harikiruban2006@gmail.com>
2026-02-01 06:21:02 -0500

    Create Looping.cpp

---
+++ C++ Learning Phases/Phase-1/Looping.cpp
@@ -0,0 +1,7 @@
+/*
+In C++, ++i (prefix) is often more efficient than i++ (postfix) because it doesn't create a temporary copy of the variable.
+
+for loop: Best for when you know exactly how many times to run (e.g., "Calculate the average for 5 assignments").
+
+while loop: Best for when you’re waiting for a condition (e.g., "Keep calculating until the user enters -1").
+*/
