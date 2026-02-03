357e1c80444111b49a579ae491363530b97ba61c
hkiruban <harikiruban2006@gmail.com>
2026-02-03 04:44:38 -0500

    Update Direct-memory modification.cpp

--- C++ Learning Phases/Phase-2/Direct-memory modification.cpp
+++ C++ Learning Phases/Phase-2/Direct-memory modification.cpp
@@ -3,3 +3,13 @@
 void applyBaneCurve(int &grade) {
     grade += 5; // Direct modification of the original memory
 }
+
+int main(){
+    int ActualGrade = 95;
+
+    applyBaneCurve(ActualGrade);
+
+    std::cout << "Final Grade: " << ActualGrade << std::endl;
+
+    return 0;
+}
